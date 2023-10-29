55862ce559a7c34d7c5d8e52b4f013bc203eede6
ahfetea <ahfetea@yahoo.com>
2023-10-29 16:21:57 -0400

    added db config

---
+++ config/config.php
@@ -0,0 +1,12 @@
+<?php
+class Config {
+    public static $db = [
+        "host" => "db",
+        "port" => 5432,
+        "user" => "localuser",
+        "pass" => "your_password",
+        "database" => "music_app"
+    ];
+}
+?>
+
---
+++ db/Database.php
@@ -0,0 +1,25 @@
+<?php
+class Database {
+    private $dbConnector;
+
+    public function __construct() {
+        $host = Config::$db["host"];
+        $user = Config::$db["user"];
+        $database = Config::$db["database"];
+        $password = Config::$db["pass"];
+        $port = Config::$db["port"];
+
+        $this->dbConnector = pg_connect("host=$host port=$port dbname=$database user=$user password=$password");
+    }
+
+    public function query($query, ...$params) {
+        $res = pg_query_params($this->dbConnector, $query, $params);
+        if ($res === false) {
+            echo pg_last_error($this->dbConnector);
+            return false;
+        }
+        return pg_fetch_all($res);
+    }
+}
+?>
+
