aa84a733bb4fea8372d5e302a52393525244ebe0
Daniel Huynh <danielhuynh523@gmail.com>
2023-11-27 11:02:07 -0500

    added login-validation.js

---
+++ scripts/login-validation.js
@@ -0,0 +1,22 @@
+
+const username = document.getElementById('username');
+const password = document.getElementById('passwd');
+const errorMessage = document.getElementById('errorMessage');
+
+username.addEventListener("oninput", validateForm);
+
+function validateForm() {
+    let username_val = username.val;
+    let password_val = password.val;
+    const hasNumber = /\d/.test(password_val);
+    
+    if (username_val == "" || password_val == "") {
+        errorMessage.innerHTML = "Please fill out all fields";
+        return false;
+    }
+    else if(hasNumber == false) {
+        errorMessage.innerHTML = "Password must contain at least one number";
+        return false;
+    }
+    return true;
+}
