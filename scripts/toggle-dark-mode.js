bbd4522e80fcdadf66964055f078276d311bbffb
Daniel Huynh <danielhuynh523@gmail.com>
2023-11-27 11:34:31 -0500

    added dark mode style to styles.css, added toggle for darkmode in js, and button for navbar in home.php

---
+++ scripts/toggle-dark-mode.js
@@ -0,0 +1,5 @@
+
+// anonymous function
+document.getElementById('darkModeToggle').addEventListener('click', function () {
+    document.body.classList.toggle('dark-mode');
+});
--- templates/login.php
+++ templates/login.php
@@ -15,7 +15,7 @@
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <link rel="stylesheet" href="/opt/src/music-social-media/styles/styles.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
-    <script src="/opt/src/music-social-media/scripts/scripts.js"></script>
+    <script src="/opt/src/music-social-media/scripts/login-validation.js"></script>
 </head>
 
 <body>
